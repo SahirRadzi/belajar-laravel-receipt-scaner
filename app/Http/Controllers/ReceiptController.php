031d24cfb92c087b7262a776003208f055215903
SahirRadzi <sahir.radzi@gmail.com>
2024-12-08 00:36:38 +0800

    setup ReceiptController

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ReceiptController extends Controller
 {
-    //
+    public function index()
+    {
+        return redirect()->route('receipts.create');
+    }
+
+    public function create()
+    {
+        return view('receipts.create');
+    }
 }
